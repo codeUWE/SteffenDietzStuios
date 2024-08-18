1e6c0e2b5cff8379e18fc8d1d9fefb608ee9882c
codeUWE <code.cayan@gmail.com>
2024-08-18 17:00:12 +0200

    first styling of exhibitions area

--- frontend/src/components/Exhibitions/Exhibitions.jsx
+++ frontend/src/components/Exhibitions/Exhibitions.jsx
@@ -1,20 +1,41 @@
 import React from "react";
 import "./Exhibitions.css";
-
 import NavbarBottom from "../NavbarBottom/NavbarBottom";
+
+//assets
+import { ImLocation } from "react-icons/im";
 
 function Exhibitions() {
 	return (
 		<>
-			<div className="dates">
-				<div className="ongoing">
-					<h2>Ongoing Exhibitions</h2>
+			<div className="exhibitions">
+				<div className="upcoming exhibitionTitle">
+					<h2>Upcoming Events</h2>
+					<div className="upcomingExhibitions">
+						<div className="upcomingEvent">
+							<div className="upcomingMain">
+								<div className="upcomingInfo">
+									<h2>Pop up art galerie</h2>
+									<p>
+										{" "}
+										Waitzstrasse 11, <br /> 22607 Hamburg
+									</p>
+								</div>
+								<div className="upcomingActionBtn">
+									<button className="upcomingViewEvent">see event</button>
+									<button className="upcomingViewLocation">
+										<ImLocation size={25} color="#d5573b" />
+									</button>
+								</div>
+							</div>
+							<div className="upcomingDate">
+								<h3>04. Jul. 2024 - 03. Aug 2024</h3>
+							</div>
+						</div>
+					</div>
 				</div>
-				<div className="upcoming">
-					<h2>Upcoming Exhibitions</h2>
-				</div>
-				<div className="previous">
-					<h2> Previous Exhibitions</h2>
+				<div className="previous exhibitionTitle">
+					<h2> Previous Events</h2>
 				</div>
 			</div>
 			<NavbarBottom />
